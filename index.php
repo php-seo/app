d88f131e29ad5c03a7163092cc06b9b6e937092b
php-seo <d777alex777@yandex.ru>
2016-05-22 16:58:23 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,3 +3,7 @@
 echo 'Валюша только моя (Твой Лешка)';
 echo "<br/>";
 echo "<img src='http://lphp.ru/images/dalmation.gif'>";
+
+echo "<br/>";echo "<br/>";echo "<br/>";
+echo "<img src='777.jpg'>";
+ 	
